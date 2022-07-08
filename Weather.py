147a54311c8e172905b6b2381921b10094252342
Yuki Iriyama <gajumaru19990110@gmail.com>
2022-07-08 10:58:46 +0900

    間違えてgitignoreにつっこまれてたやつを再コミット

---
+++ Weather.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import datetime
+import json
+import pprint
+import InfoGetterIF
+
+class weatherGetter(InfoGetterIF.InfoGetterIF):
+    def _gatherData(self):
+        url = "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m&hourly=precipitation&timezone=Asia%2FTokyo"
+        res = requests.get(url)
+        data = json.loads(res.text)
+        city = "Tokyo"
+        temp_hi = max(data["hourly"]["temperature_2m"][24:48])
+        temp_lo = min(data["hourly"]["temperature_2m"][24:48])
+        prcp = max(data["hourly"]["precipitation"][:24])
+        date = str(datetime.date.today() + datetime.timedelta(days=1))
+
+        # DBに格納
+        queryDict = {
+            "data" : [
+                ["city", "\'"+city+"\'"],
+                ["temp_lo", str(temp_lo)],
+                ["temp_hi", str(temp_hi)],
+                ["prcp", str(prcp)],
+                ["date", "\'"+date+"\'"]
+            ],
+            "table" : "weather"
+        }       
+        return queryDict
